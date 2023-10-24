e803d6cb1d2350a3ccabdf5137dd65fa6ca130b7
Farshad <farshad.tofighi74@gmail.com>
2023-10-24 10:03:56 +0330

    prepare service provider

--- src/DotEnvServiceProvider.php
+++ src/DotEnvServiceProvider.php
@@ -7,5 +7,23 @@ use Illuminate\Support\ServiceProvider;
 
 class DotEnvServiceProvider extends ServiceProvider
 {
+    /**
+     * register provider
+     *
+     * @return void
+     */
+    public function register()
+    {
 
+    }
+
+    /**
+     * boot provider
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+
+    }
 }
